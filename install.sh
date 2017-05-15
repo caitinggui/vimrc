d7abb4ed8fd4c98aea5150f84474a96f370a255b
caitinggui <1029645297@qq.com>
2017-05-15 15:45:40 +0800

    add ack installing

--- install.sh
+++ install.sh
@@ -48,15 +48,18 @@ else
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 fi
 
-# For YouCompleteMe and python-dev
+# For YouCompleteMe, python-dev and ctrlsf
 if [ $OS = 'centos' ]
 then
 	$os_install install -y cmake;
 	$os_install groupinstall -y "Development Tools";
+    $os_install install -y ack
+
 elif [ $OS = 'debian' ]
 then
     $os_install install -y python-dev python3-dev;
     $os_install install -y build-essential cmake;
+    $os_install install -y ack-grep
 fi
 
 # install basic environment
@@ -74,8 +77,6 @@ sudo pip install autopep8
 sudo pip install flake8
 # For tagbar
 $os_install install -y ctags
-# For ctrlsf: global character search
-$os_install install -y ack
 
 # install vim plugin
 echo 'install vim plugin, and it will cost some minutes'
